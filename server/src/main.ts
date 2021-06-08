accb8df0137a4eade217307520f69720744f0a19
v0ldemar01 <voldemarprk2001@gmail.com>
2021-06-08 21:07:06 +0300

    Add api prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT');
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   await app.listen(PORT);
 }
 bootstrap();
